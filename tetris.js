b3bc9860963b8d84595eb1e07aded93258caf0ed
Tyler Lawson <lawsonty@oregonstate.edu>
2018-12-14 20:19:43 -0800

    Added tick method

--- tetris.js
+++ tetris.js
@@ -2,16 +2,9 @@
 const X_BOUND = 30
 const Y_BOUND = 50
 
-//Game state
-//Tetraminoes is the list of all floating tetraminoes
-//free_blocks is the list of points no longer part of floating tetraminoes
-var state = {
-    tetraminoes: [],
-    free_blocks: []
-}
-
 //Define the types of shapes
-var shapes = ['I', 'O', 'T', 'L', 'J', 'S', 'Z']
+const shapes = ['I', 'O', 'T', 'L', 'J', 'S', 'Z']
+
 //Define the colors fo each shape
 const colors = {
     I:  "Cyan",
@@ -34,6 +27,45 @@ const squares = {
     Z:  [[-1, -1], [0, -1], [0, 0],  [1, 0]]
 }
 
+//Game state
+//Tetraminoes is the list of all floating tetraminoes
+//free_blocks is the list of points no longer part of floating tetraminoes
+var state = {
+    tetraminoes: null,
+    next: [Tetramino(shapes[math.floor(math.random() * 7)])],
+    free_blocks: [],
+    ticks:  0,
+    tick: function(){
+        if(tetraminoes == null) {
+            var s = shapes[math.floor(math.random() * 7)]
+            state.tetraminoes = Tetramino(s)
+        } else {
+            var coll = state.tetraminoes.translate(0, -1)
+            if(coll == 2){
+                state.tetraminoes.get_points().forEach( p => {
+                    state.free_blocks[p[1]][p[0]] = [1, state.tetraminoes.color]
+                })
+                state.tetraminoes = null
+            }
+        }
+        ticks += 1
+        return
+    },
+    get_points: () => {
+        out = []
+        for(var i = 0; i < Y_BOUND; i++){
+            for(var k = 0; k < X_BOUND; k++){
+                if(state.free_blocks[i][k][0] == 1){
+                    out.push(Free_Block(k, i, state.free_blocks[i][k][1]))
+                }
+            }
+        }
+        return out
+    }
+}
+
+
+
 //Tetramino constructor
 function Tetramino(shape){
     this.color = colors[shape];
@@ -47,19 +79,27 @@ function Tetramino(shape){
             this.points[i] = math.multiply(rotation, this.points[i]);
         }
     };
+    //Translates tetramino by x and y, checking for a collision first.
+    //0 == collision, 1 == side collision, 2 == bottom collision
     this.translate = function(x, y) {
-        collision = false
+        var collision = 0
         this.points.forEach(
             function(p) {
-                if(state.free_blocks[this.pos.y + y + p[1]][this.pos.x + x + p[0]][0] == 1)
-                    collision = true;
+                var new_x = this.pos.x + x + p[0]
+                var new_y = this.pos.y + y + p[1]
+                if(new_x > X_BOUND || new_x < 0 || state.free_blocks[new_y][new_x][0] == 1){
+                    collision = 1;
+                    return;
+                }
+                if(new_y < 0 || state.free_blocks[new_y][new_x][0] == 1)
+                    collision = 2;
             }
         );
-        if(!collision){
+        if(collision == 0){
             this.pos.x += x;
             this.pos.y += y;
         }
-        return !collision
+        return collision
     };
     //Return a list of points representing each of the tetraminoes
     //squares
