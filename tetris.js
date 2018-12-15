bdc557d12896dce4fad2c987e868db160963708c
Tyler Lawson <lawsonty@oregonstate.edu>
2018-12-14 21:01:20 -0800

    Changed function constructors to classes

--- tetris.js
+++ tetris.js
@@ -27,18 +27,79 @@ const squares = {
     Z:  [[-1, -1], [0, -1], [0, 0],  [1, 0]]
 }
 
+
+
+
+//Tetramino constructor
+class Tetramino{
+    constructor(shape) {
+        this.color = colors[shape];
+        this.rotation = 0;
+        this.pos = {x: 15, y: 50};
+        this.shape = shape;
+        this.points = squares[shape];
+    }
+    rotate() {
+        var rotation = math.matrix([[0,-1],[1,0]]);
+        for(i = 0; i < this.points.length; i++){
+            this.points[i] = math.multiply(rotation, this.points[i]);
+        }
+    };
+    //Translates tetramino by x and y, checking for a collision first.
+    //0 == collision, 1 == side collision, 2 == bottom collision
+    translate(x, y) {
+        var collision = 0
+        this.points.forEach(
+            (p) => {
+                var new_x = this.pos.x + x + p[0]
+                var new_y = this.pos.y + y + p[1]
+                if(new_x > X_BOUND || new_x < 0 || state.free_blocks[new_y][new_x][0] == 1){
+                    collision = 1;
+                    return;
+                }
+                if(new_y < 0 || state.free_blocks[new_y][new_x][0] == 1)
+                    collision = 2;
+            }
+        );
+        if(collision == 0){
+            this.pos.x += x;
+            this.pos.y += y;
+        }
+        return collision
+    };
+    //Return a list of points representing each of the tetraminoes
+    //squares
+    get_points(){
+        var out = [];
+        this.points.forEach( (p) => {
+            out.push([this.pos.x + p[0], thix.pos.y + p[2]]);
+        })
+        return out;
+    }
+}
+
+class Free_Block{
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color
+    }
+}
+
+
 //Game state
 //Tetraminoes is the list of all floating tetraminoes
 //free_blocks is the list of points no longer part of floating tetraminoes
 var state = {
     tetraminoes: null,
-    next: [Tetramino(shapes[math.floor(math.random() * 7)])],
+    next: [new Tetramino(shapes[math.floor(math.random() * 7)])],
     free_blocks: [],
     ticks:  0,
     tick: function(){
-        if(tetraminoes == null) {
+        if(state.tetraminoes == null) {
             var s = shapes[math.floor(math.random() * 7)]
-            state.tetraminoes = Tetramino(s)
+            state.tetraminoes = state.next[state.next.length - 1]
+            state.next[state.next.length - 1] = new Tetramino(s)
         } else {
             var coll = state.tetraminoes.translate(0, -1)
             if(coll == 2){
@@ -48,7 +109,7 @@ var state = {
                 state.tetraminoes = null
             }
         }
-        ticks += 1
+        state.ticks += 1
         return
     },
     get_points: () => {
@@ -70,62 +131,6 @@ var state = {
         return out
     }
 }
-
-
-
-//Tetramino constructor
-function Tetramino(shape){
-    this.color = colors[shape];
-    this.rotation = 0;
-    this.pos = {x: 15, y: 50};
-    this.shape = shape;
-    this.points = squares[shape];
-    this.rotate = function() {
-        var rotation = math.matrix([[0,-1],[1,0]]);
-        for(i = 0; i < this.points.length; i++){
-            this.points[i] = math.multiply(rotation, this.points[i]);
-        }
-    };
-    //Translates tetramino by x and y, checking for a collision first.
-    //0 == collision, 1 == side collision, 2 == bottom collision
-    this.translate = function(x, y) {
-        var collision = 0
-        this.points.forEach(
-            function(p) {
-                var new_x = this.pos.x + x + p[0]
-                var new_y = this.pos.y + y + p[1]
-                if(new_x > X_BOUND || new_x < 0 || state.free_blocks[new_y][new_x][0] == 1){
-                    collision = 1;
-                    return;
-                }
-                if(new_y < 0 || state.free_blocks[new_y][new_x][0] == 1)
-                    collision = 2;
-            }
-        );
-        if(collision == 0){
-            this.pos.x += x;
-            this.pos.y += y;
-        }
-        return collision
-    };
-    //Return a list of points representing each of the tetraminoes
-    //squares
-    this.get_points = function(){
-        var out = [];
-        this.points.forEach( function(p){
-            out.push([this.pos.x + p[0], thix.pos.y + p[2]]);
-        })
-        return out;
-    }
-}
-
-function Free_Block(x, y, color){
-    this.x = x;
-    this.y = y;
-    thix.color = color
-}
-
-
 for(var i = 0; i < Y_BOUND; i++){
     state.free_blocks.push([])
     for(var k = 0; k < X_BOUND; k++){
