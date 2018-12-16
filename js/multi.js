0994c6fa41dca9e09f44ec7d475157a25be4d510
Chris <celticchristoph@live.com>
2018-12-16 05:25:57 -0800

    Socket emitting and drawing to 'opponent' board.

--- js/multi.js
+++ js/multi.js
@@ -1,1 +1,19 @@
 var socket = io.connect('http://71.193.188.235:8080/');
+var oppBoard = null;
+
+function onPageLoaded(obj) {
+  oppBoard = obj;
+  console.log(oppBoard);
+}
+
+function sendBoard(board) {
+  // console.log(board);
+  socket.emit('board', {
+    oppBoard: board,
+  });
+}
+
+socket.on('board', function(data) {
+  console.log(data.oppBoard);
+  oppBoard.innerHTML = data.oppBoard;
+});
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) =>
 res.sendFile(path.join(__dirname + "/public/index.html"))
 );
 
+app.get("/multi.html", (req, res) =>
+res.sendFile(path.join(__dirname + "/public/multi.html"))
+);
+
 app.get("/js/tetris.js", (req, res) =>
 res.sendFile(path.join(__dirname + "/js/tetris.js"))
 );
@@ -25,4 +29,10 @@ var io = socket(app.listen(port, () => console.log(`Up on port ${port}!`)));
 
 io.on('connection', function(socket) {
   console.log(`Socket connection made by ${socket.id}`);
+
+  socket.on('board', function(data) {
+    // console.log(data);
+    // console.log("Data recived.");
+    io.sockets.emit('board', data);
+  });
 });
